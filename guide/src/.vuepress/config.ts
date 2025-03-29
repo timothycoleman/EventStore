53544b7fcb28d026a10319ca2c3bd310e0f0f6d6
Timothy Coleman <timothy.coleman@gmail.com>
2025-03-29 10:15:29 +0000

    wip

--- guide/src/.vuepress/config.ts
+++ guide/src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/guide/", //qq
+  base: "/",
 
   lang: "en-US",
   title: "KurrentDB Contributors Guide",
